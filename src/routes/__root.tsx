0d5d775e67e6bb46a65895154e36dcde12a8e9cb
mintykiera <85643272+mintykiera@users.noreply.github.com>
2026-07-24 23:55:34 +0800

    rf: blueboard logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/blueboard-removebg-preview.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
